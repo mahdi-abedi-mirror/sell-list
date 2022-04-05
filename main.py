bdc512193a20eaf0da0a10cc9bb5658b9ce9efd3
mahdi <abedimhmahdi@gmail.com>
2022-04-05 23:19:26 +0430

    created menu

--- main.py
+++ main.py
@@ -3,7 +3,20 @@
 
 import sqlite3      #import sqlite3 library
 from sqlite3 import Error
-from time import sleep
+
+con = sqlite3.connect('order.db')
+cur = con.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS orders(
+    Id INT PRIMARY KEY,
+    fname TEXT,
+    lname TEXT,
+    date TEXT,
+    total INT);
+""")
+con.commit()
+star="*"*30
+
+
 
 def sql_connection():       # Tese connect or created database
 
@@ -21,21 +34,54 @@ def sql_connection():       # Tese connect or created database
 
         con.close()
 
-sql_connection()
-sleep(5)
-con = sqlite3.connect('order.db')
-cur = con.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS orders(
-    Id INT PRIMARY KEY,
-    fname TEXT,
-    lname TEXT,
-    date TEXT,
-    total INT);
-""")
-con.commit()
 
+class menu(object):
+    def one(self):
+        pass
+    def two(self):
+        return print("two")
+    def three(self):
+        pass
+    def four(self):
+        pass
+    def five(self):
+        pass
+    def six(self):
+        print("*"*10,"GoodBy","*"*10)
+def chooser():
+    print(star," S E L L - L I S T ",star)
+    print(star," M A I N - M E N U ",star)
+    print("*"*10," please choise ","*"*10)
+    print(""" 
+    1 . Add Order \t 2 . Show All Orders \n 
+    3 . Show By Id \t 4 . Delet By Id \n
+    5 . Total Sell \t 6 . Exite \n""")
+    choose=input("please enter your choose : ")
+    if choose == 1:
+        menu().one()
+    elif choose == 2:
+        menu().two()
+    elif choose == 3:
+        menu().three()
+    elif choose == 4:
+        menu().four()
+    elif choose == 5:
+        menu().five()
+    elif choose == 6:
+        menu().six()
+    else:
+        print("your choose invalid")
 
-
+loop=True
+chooser()
+while loop:
+    endornot = input("are you want closed !!! answer By y or n : ")
+    if endornot == "n" :
+        chooser()
+    elif endornot =="y":
+        loop=False
+    else :
+        print("your choose invalid")
 
 
 con.close()
