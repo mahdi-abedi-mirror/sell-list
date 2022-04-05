c89724ecec80316d0e7e64be93bff6dc1d16c827
mahdi <abedimhmahdi@gmail.com>
2022-04-06 01:34:36 +0430

    enter command menu

--- main.py
+++ main.py
@@ -3,55 +3,76 @@
 
 import sqlite3      #import sqlite3 library
 from sqlite3 import Error
+from os import system, name
+
+def osclear():
+   # for windows
+   if name == 'nt':
+        system('cls')
+
+   # for mac and linux
+   else:
+        system('clear')
 
 con = sqlite3.connect('order.db')
 cur = con.cursor()
 cur.execute("""CREATE TABLE IF NOT EXISTS orders(
-    Id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY UNIQUE,
     fname TEXT,
-    lname TEXT,
+    lname TEXT ,
     date TEXT,
     total INT);
 """)
 con.commit()
-star="*"*30
-
-
-
-def sql_connection():       # Tese connect or created database
-
-    try:
-
-        con = sqlite3.connect('order.db')
-
-        print("Connection is established")
-
-    except Error:
-
-        print(Error)
-
-    finally:
-
-        con.close()
-
 
 class menu(object):
+
     def one(self):
-        pass
+
+        cur.execute("SELECT id FROM orders")
+        last_id=cur.fetchall()
+        print(" Last registered ID = ",last_id)
+        Id=input("Please Enter Id for new order : ")
+        fname=input("Please Enter First Name Customer : ")
+        lname=input("Please Enter Last Name Customer : ")
+        date_sell=input("Please Enter Date Ordered : ")
+        total=input("Please Enter Total Order Amount : ")
+        data_for_db=(Id,fname,lname,date_sell,total)
+        cur.execute("INSERT INTO orders VALUES(?,?,?,?,?)",data_for_db)
+        con.commit()
+        con.close()
+
     def two(self):
-        return print("two")
+        
+        cur.execute("SELECT * FROM orders")
+        rows = cur.fetchall()
+        print("id | fname | lname | date-order | order-Amont |")
+        for row in rows:
+            print(row,"\n")
+
     def three(self):
-        pass
+        
+        sh_id=int(input("Please Enter Id : "))
+        cur.execute("SELECT * FROM orders WHERE id = %i"%sh_id)
+        print(cur.fetchall())
+
     def four(self):
-        pass
+        
+        del_id=int(input("Please Enter Id For Delete : "))
+        cur.execute("DELETE FROM orders WHERE id = %i"%del_id)
+        con.commit()
+        print("Deleted!!!")
+
     def five(self):
         pass
     def six(self):
         print("*"*10,"GoodBy","*"*10)
+
+
 def chooser():
-    print(star," S E L L - L I S T ",star)
-    print(star," M A I N - M E N U ",star)
-    print("*"*10," please choise ","*"*10)
+    print("*"*30," S E L L - L I S T ","*"*30)
+    print("*"*30," M A I N - M E N U ","*"*30)
+    print("*"*10," please choose ","*"*10)
     print(""" 
     1 . Add Order \t 2 . Show All Orders \n 
     3 . Show By Id \t 4 . Delet By Id \n
@@ -73,12 +94,15 @@ def chooser():
         print("your choose invalid")
 
 loop=True
+osclear()
 chooser()
 while loop:
     endornot = input("are you want closed !!! answer By y or n : ")
     if endornot == "n" :
+        osclear()
         chooser()
     elif endornot =="y":
+        print("*"*10,"GoodBy","*"*10)
         loop=False
     else :
         print("your choose invalid")
